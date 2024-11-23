9758da188d3febdec199986abf9c799bd58c758a
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-11-23 20:26:50 +0500

    first counter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,79 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const colorScheme = useColorScheme();
+	const [loaded] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+	});
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	const styles = StyleSheet.create({
+		headerText: {
+			fontFamily: "arial",
+			textAlign: "center",
+		},
+
+		buttonStyle: {
+			padding: 20,
+			backgroundColor: "#007bff",
+			color: "white",
+			fontSize: 20,
+			borderRadius: 10,
+			width: 200,
+			height: 50,
+			justifyContent: "center",
+			alignItems: "center",
+		},
+
+		buttonContainer: {
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+
+		countContainer: {
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+			margin: 20,
+		},
+	});
+	const [count, setCount] = useState(0);
+
+	return (
+		<View>
+			<Text style={styles.headerText}>Hello World React Native !</Text>
+
+			<View style={styles.countContainer}>
+				<Text>{count}</Text>
+			</View>
+
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					onPress={() => setCount(count + 1)}
+					style={styles.buttonStyle}
+				>
+					Click me
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
